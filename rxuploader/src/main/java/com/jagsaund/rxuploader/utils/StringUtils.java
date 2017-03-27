3fef58af526dcdd81201200ad02f86bf8b4cde76
Jag Saund <jag.saund@alumni.utoronto.ca>
2017-03-27 09:31:43 -0700

    Add StringUtils Helper Methods

---
+++ rxuploader/src/main/java/com/jagsaund/rxuploader/utils/StringUtils.java
@@ -0,0 +1,20 @@
+package com.jagsaund.rxuploader.utils;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+public class StringUtils {
+    public static boolean isNullOrEmpty(@Nullable CharSequence s) {
+        return s == null || s.length() == 0;
+    }
+
+    @NonNull
+    public static String getOrDefault(@Nullable String s, @NonNull String t) {
+        return s != null ? s : t;
+    }
+
+    @NonNull
+    public static String getOrEmpty(@Nullable String s) {
+        return getOrDefault(s, "");
+    }
+}
