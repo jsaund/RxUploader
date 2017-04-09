22ed5114fb75f68d0a672e00274a9e112da1e0a9
Jag Saund <jag.saund@alumni.utoronto.ca>
2017-04-08 20:44:41 -0700

    Add documentation to UploadService

--- rxuploader/src/main/java/com/jagsaund/rxuploader/store/UploadService.java
+++ rxuploader/src/main/java/com/jagsaund/rxuploader/store/UploadService.java
@@ -5,6 +5,18 @@ import java.util.Map;
 import okhttp3.MultipartBody;
 import rx.Single;
 
+/**
+ * Uploads a multipart entity and optional metadata contents.
+ *
+ * @param <T> Defines the type of response payload returned by the upload operation
+ */
 public interface UploadService<T> {
+    /**
+     * Uploads multipart content to a remote endpoint.
+     *
+     * @param metadata Optional information to be associated with the upload operation
+     * @param data A file to upload
+     * @return The response received from the upload operation
+     */
     Single<T> upload(@NonNull Map<String, Object> metadata, @NonNull MultipartBody.Part data);
 }
